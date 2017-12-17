c22b8b2fb518f7f470a627c017c68370e191cc26
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-12-17 21:42:50 +0100

    using a file

--- src/GraphQL.Server.AspNetCore.GraphiQL/Internal/GraphiQLHTMLSettings.cs
+++ src/GraphQL.Server.AspNetCore.GraphiQL/Internal/GraphiQLHTMLSettings.cs
@@ -1,12 +1,21 @@
+using System.IO;
+using System.Reflection;
+
 namespace GraphQL.Server.AspNetCore.GraphiQL.Internal {
 
 	internal class GraphiQLHTMLSettings {
 
-		private static readonly string graphiQLCSHtml="";
+		private static string graphiQLCSHtml;
 
 		public static string HTML {
 			get {
-				return graphiQLCSHtml;
+				if (graphiQLCSHtml != null) {
+					return graphiQLCSHtml;
+				}
+				var assembly = typeof(GraphiQLHTMLSettings).GetTypeInfo().Assembly;
+				var resource = assembly.GetManifestResourceStream("GraphQL.Server.AspNetCore.GraphiQL.Internal.graphiql.cshtml");
+				graphiQLCSHtml= new StreamReader(resource).ReadToEnd();
+				return HTML;
 			}
 		}
 	}
