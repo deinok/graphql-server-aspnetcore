f89dd993fb06059cf3cf90c021a93660ea4671f1
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-12-17 01:23:37 +0100

    GraphQLTests

--- tests/TestingWebApp/Startup.cs
+++ tests/TestingWebApp/Startup.cs
@@ -1,5 +1,9 @@
+using GraphQL;
+using GraphQL.Http;
 using GraphQL.Server.AspNetCore;
 using GraphQL.StarWars;
+using GraphQL.StarWars.Types;
+using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,6 +16,21 @@ namespace TestingWebApp {
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services) {
+			services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
+			services.AddSingleton<IDocumentWriter, DocumentWriter>();
+
+			services.AddSingleton<StarWarsData>();
+			services.AddSingleton<StarWarsQuery>();
+			services.AddSingleton<StarWarsMutation>();
+			services.AddSingleton<HumanType>();
+			services.AddSingleton<HumanInputType>();
+			services.AddSingleton<DroidType>();
+			services.AddSingleton<CharacterInterface>();
+			services.AddSingleton<EpisodeEnum>();
+			services.AddSingleton<ISchema>(
+				s => new StarWarsSchema(new FuncDependencyResolver(type => (GraphType)s.GetService(type))));
+
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,7 +40,7 @@ namespace TestingWebApp {
 			}
 
 			app.UseGraphQLServer(new GraphQLMiddlewareSettings {
-				Schema = new StarWarsSchema()
+				Schema = app.ApplicationServices.GetRequiredService<ISchema>()
 			});
 
 			app.Run(async (context) => {
